d92702270d77bc5513222eb3967bd4cb39df5319
Favor Charles Owuor <133737127+f18charles@users.noreply.github.com>
2026-08-24 20:55:49 +0300

    refactor: export stores and types from index file
    Consolidate store imports and exported types into the central index file to simplify module access throughout the application.

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+export { useAuthStore } from "./useAuthStore.ts";
+export { useTaskStore } from "./useTaskStore.ts";
+export type { Task, Subtask } from "./useTaskStore.ts";
+export { useHabitStore } from "./useHabitStore.ts";
+export type { Habit } from "./useHabitStore.ts";
+export { useEventStore } from "./useEventStore.ts";
+export type { Event } from "./useEventStore.ts";
+export { useToastStore } from "./useToastStore.ts";
+export type { ToastMessage, ToastType } from "./useToastStore.ts";
